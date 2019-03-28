c57b34e1bc89bca711ad53f1cc61fa9f82c608fe
gwcfate <2453316835@qq.com>
2019-03-28 16:47:59 +0800

    登陆静态

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,62 @@
 <template>
-  <div class="login">登陆</div>
+  <div class="login">
+    <el-form ref="form" :model="form" label-width="80px">
+      <img src="../assets/images/313263.jpg" alt>
+      <el-form-item label="用户">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登陆</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
-<style>
+<style lang='less'>
+.login {
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+
+  .el-form {
+    width: 500px;
+    background-color: #fff;
+    margin: 200px auto;
+    padding: 75px 40px 15px;
+    border-radius: 20px;
+    position: relative;
+
+    img {
+      width: 100px;
+      height: 100px;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -50px;
+      border-radius: 50%;
+      border: 10px solid #fff;
+    }
+
+    .el-button + .el-button {
+      margin-left: 80px;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+// 引入公共样式
+import '@/assets/common.css'
 
 Vue.config.productionTip = false
 
