5844d766b7b4b9416d4722138be50574e5721b6c
gwcfate <2453316835@qq.com>
2019-06-14 14:14:19 +0800

    完成了home组件功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,128 @@
 <template>
-  <div class="home">home组件</div>
+  <el-container class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="logout">
+        欢迎光临
+        <a href="javascript:;" @click="logout">退出</a>
+      </div>
+      <h1 class="title">电商后台管理系统</h1>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="1-1"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <!-- 放菜单项 -->
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <!-- 放菜单项 -->
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    //退出
+    logout() {
+      //弹出模态框
+      this.$confirm("你确定要推出么？", "温馨提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "info"
+      })
+        .then(() => {
+          //点确认
+          localStorage.removeItem("token");
+          this.$router.push("/login");
+          this.$message.success("退出成功");
+        })
+        .catch(() => {
+          this.$message.info("取消退出");
+        });
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+// 如果给style添加scoped属性，样式只会在当前组件生效
+// 实现原理： 如果给当前组件的style添加了scoped,,,,把当前组件中所有的div都增加一个随机的属性
+.home {
+  height: 100%;
+  .el-header {
+    background-color: #b3c1cd;
+
+    .logo {
+      width: 180px;
+      height: 60px;
+      float: left;
+      background-image: url("../assets/logo.png");
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+    .logout {
+      width: 180px;
+      height: 60px;
+      float: right;
+      line-height: 60px;
+      text-align: right;
+      font-weight: 700;
+      a {
+        color: darkorange;
+      }
+    }
+
+    .title {
+      height: 60px;
+      overflow: hidden;
+      text-align: center;
+      line-height: 60px;
+      color: #fff;
+      font-size: 30px;
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+
+    .el-submenu {
+      width: 200px;
+    }
+  }
+  .el-main {
+    background-color: #d4dfe4;
+  }
+}
 </style>
